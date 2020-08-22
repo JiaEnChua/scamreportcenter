426efeee10ba9acfacb456dbc39f9e26003e1982
jiaenchua <jiaen_je@hotmail.com>
2020-08-22 14:40:42 -0500

    Remove comments when seeding

--- seeds.js
+++ seeds.js
@@ -3,47 +3,56 @@ var Review = require("./models/review");
 var Comment = require("./models/comment");
 
 var data = [
-    {
-        title: "Shopthelike.com"
-    },
-    {
-        title: "Facebook.com"
-    },
-    {
-        title: "google.com"
-    }
-]
-
+  {
+    title: "Shopthelike.com",
+  },
+  {
+    title: "Facebook.com",
+  },
+  {
+    title: "google.com",
+  },
+];
 
 function seedDB() {
-    Review.deleteMany({}, function(err) {
-        if(err) {
+  Review.deleteMany({}, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Removed all reviews");
+
+      data.forEach(function (seed) {
+        Review.create(seed, function (err, newlyCreatedReview) {
+          if (err) {
             console.log(err);
-        } else {
-            console.log("Removed all reviews");
-            data.forEach(function(seed){
-                Review.create(seed, function(err, newlyCreatedReview) {
-                    if (err){
-                        console.log(err);
+          } else {
+            console.log("added a new review " + seed.title);
+            Comment.deleteMany({}, function (err) {
+              if (err) {
+                console.log(err);
+              } else {
+                Comment.create(
+                  {
+                    text: "This company is maybe a scam!",
+                    author: "Homer",
+                  },
+                  function (err, comment) {
+                    if (err) {
+                      console.log(err);
                     } else {
-                        console.log("added a new review "+seed.title);
-                        Comment.create({
-                            text: "This company is maybe a scam!",
-                            author: "Homer"
-                        }, function(err, comment){
-                            if (err) {
-                                console.log(err);
-                            } else {
-                                newlyCreatedReview.comments.push(comment);
-                                newlyCreatedReview.save();
-                                console.log("New comment added");
-                            }
-                        })
+                      newlyCreatedReview.comments.push(comment);
+                      newlyCreatedReview.save();
+                      console.log("New comment added");
                     }
-                });
+                  }
+                );
+              }
             });
-        }
-    })
+          }
+        });
+      });
+    }
+  });
 }
 
 module.exports = seedDB;
